8667eaf7f9fc00c9e3a74a59901d31f6f71447ff
hernandez87v <hernandez87v@hotmail.com>
2020-06-16 18:17:31 -0700

    Program with numbers → Good rest on vacation - Not yet complete

---
+++ Problems/Good rest on vacation/task.py
@@ -0,0 +1,7 @@
+# put your python code here
+duration_days = int(input())
+total_food_cost = int(input())
+flight_cost = int(input())
+one_night_hotel = int(input())
+
+print((duration_days * total_food_cost + one_night_hotel) + flight_cost)
